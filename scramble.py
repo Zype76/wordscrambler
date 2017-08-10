55150b03be6cb07295b00be4b6f57700a4782161
Paul Hansen <phansen@datto.com>
2017-08-10 05:03:33 -0400

    Made a few changes to prevent duplicate letters

--- scramble.py
+++ scramble.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import random 
+import random
 
-def jumbles(): 
-	word = input("What's the word? ")
-	listword = list(word)
-	long = len(word)
-	letterlist = [] 
-	for x in range(0, len(word)):
-		lol = random.randint(0,len(word) )
-		letterlist.append(listword[lol-1])
-	print(''.join(letterlist))
+def jumbles():
+        word = input("What's the word? ")
+        listword = list(word)
+        long = len(word)
+        letterlist = []
+        while 0 < long:
+                lol = random.randint(0,long)
+                letterlist.append(listword[lol-1])
+                del listword[lol-1]
+                long = long-1
+        print(''.join(letterlist))
 
 jumbles()
